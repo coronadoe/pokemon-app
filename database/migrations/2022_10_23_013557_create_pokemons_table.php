bb542fb5930fc71605d88007ad582088915e6039
emmanuel <emmanuel.c.coronado@gmail.com>
2022-10-22 22:11:08 -0400

    Added table fields

--- database/migrations/2022_10_23_013557_create_pokemons_table.php
+++ database/migrations/2022_10_23_013557_create_pokemons_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePokemonTable extends Migration
+class CreatePokemonsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -15,6 +15,16 @@ class CreatePokemonTable extends Migration
     {
         Schema::create('pokemons', function (Blueprint $table) {
             $table->id();
+            $table->string('name')->nullable(false)->unique();
+            $table->integer('total')->nullable(false);
+            $table->integer('hp')->nullable(false);
+            $table->integer('attack')->nullable(false);
+            $table->integer('defence')->nullable(false);
+            $table->integer('special_attack')->nullable(false);
+            $table->integer('special_defence')->nullable(false);
+            $table->integer('speed')->nullable(false);
+            $table->integer('generation')->nullable(false);
+            $table->boolean('legendary')->default(false);
             $table->timestamps();
         });
     }
--- database/migrations/2022_10_23_013628_create_types_table.php
+++ database/migrations/2022_10_23_013628_create_types_table.php
@@ -15,6 +15,7 @@ class CreateTypesTable extends Migration
     {
         Schema::create('types', function (Blueprint $table) {
             $table->id();
+            $table->string('type_name')->nullable(false)->unique();
             $table->timestamps();
         });
     }
